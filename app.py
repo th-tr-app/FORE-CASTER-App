6ba5a0292b6726b4cdcfb9a89dae0cd402c11200
th-tr-app <t.hrs001@gmail.com>
2025-12-25 16:04:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ def fetch_market_info():
 # タイトルエリア
 st.markdown("""
     <div style='margin-bottom: 20px;'>
-        <h1 class='main-title'>FORE CASTER</h1>
-        <h3 class='sub-title'>SCREENING & BACKTEST | ver 1.1</h3>
+        <h1 style='font-weight: 400; font-size: 46px; margin: 0; padding: 0;'>FORE CASTER</h1>
+        <h3 style='font-weight: 300; font-size: 20px; margin: 0; padding: 0; color: #aaaaaa;'>SCREENING & BACKTEST | ver 1.1</h3>
     </div>
     """, unsafe_allow_html=True)
 
