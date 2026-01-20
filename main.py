36bf7c0d632cda421817ffed5d4570e45e8b89c3
th-tr-app <t.hrs001@gmail.com>
2026-01-20 11:14:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -181,8 +181,7 @@ with tab_top:
             <div style="margin-bottom: 5px; font-size: 0.8em;"><b>相場展望：</b> {diag['phase_comment']}</div>
             <div style="margin-bottom: 15px; font-size: 0.8em;"><b>米国株の影響：</b> {diag['us_impact']}</div>
             <h4 style="margin-top: 0; margin-bottom: 0px; color: #3498db; font-size: 1.0em;">👀 指標から推測できる注目セクター</h4>
-            
-            {tips_str}
+            <div style="margin-bottom: 15px; font-size: 0.8em;">{tips_str}</div>
         </div>
         <div style="height: 10px;"></div>
         """
