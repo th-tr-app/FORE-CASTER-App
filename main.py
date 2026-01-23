a12c29398c875028d06bfb14744c84342793d9f0
th-tr-app <t.hrs001@gmail.com>
2026-01-23 23:25:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def local_css(file_name):
 local_css("style.css")
 
 # --- 3. サイドバー設定 (戦略プリセット + バックテスト設定) ---
-st.sidebar.header("🎲 戦略プリセット")
+st.sidebar.header("🎲 戦略実行プリセット")
 for p, l in [("NORMAL","通常フィルター"), ("DEFENSIVE","ディフェンシブ"), ("RANGE","横ばい相場対応")]:
     is_sel = (st.session_state['preset'] == p)
     # 重複エラーを防ぐためキーを一意にする
