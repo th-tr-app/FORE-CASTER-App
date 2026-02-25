b9d5f97fa27bff36fbba0bdead34baa1bc898cf8
th-tr-app <t.hrs001@gmail.com>
2026-02-25 15:52:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ st.markdown(
     [data-testid="stHeader"] {{
         background-image: url("data:image/png;base64,{img_base64}");
         background-repeat: no-repeat;
-        background-size: 180px; /* ロゴのサイズ */
+        background-size: 200px; /* ロゴのサイズ */
         background-position: 80px center; /* 左端から20px、上下中央に配置 */
         background-color: rgba(0,0,0,0); /* バー自体の背景を透明に維持 */
     }}
