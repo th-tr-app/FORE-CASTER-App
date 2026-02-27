d526316bc9e1a8627252668a2b36af74be1baa35
th-tr-app <t.hrs001@gmail.com>
2026-02-27 16:40:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import logic_core as core
 
 # --- 1. ページ設定 & セッション管理 (永続化の定義) ---
 st.set_page_config(page_title="FORE CASTER", page_icon="image_12.png", layout="wide")
-st.logo("image_16.png", icon_image="image_16.png")
+st.logo("image_16.png", icon_image="image_12.png")
 
 # 基本情報の初期化
 if 'target_tickers' not in st.session_state: st.session_state['target_tickers'] = ""
