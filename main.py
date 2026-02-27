d5f015be046b98d680391f79f2eaf05a8cb22175
th-tr-app <t.hrs001@gmail.com>
2026-02-27 12:13:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ st.markdown(
         background-image: url("data:image/png;base64,{img_base64}");
         background-repeat: no-repeat;
         background-size: 150px; /* ロゴのサイズ */
-        background-position: 110px center; /* 左端から110px、上下中央に配置 */
+        background-position: center; /* 左端から110px、上下中央に配置 */
     }}
     </style>
     """,
