cd32dec3fc2f67510a4184b064f22a0a0e8d67b3
th-tr-app <t.hrs001@gmail.com>
2026-02-27 12:02:35 +0900

    Adjust background position in main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ st.markdown(
         background-image: url("data:image/png;base64,{img_base64}");
         background-repeat: no-repeat;
         background-size: 150px; /* ロゴのサイズ */
-        background-position: 120px center; /* 左端から40px、上下中央に配置 */
+        background-position: 100px center; /* 左端から40px、上下中央に配置 */
     }}
     </style>
     """,
