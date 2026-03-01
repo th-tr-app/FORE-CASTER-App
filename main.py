4b59fbb1766c1f892df86a7a5ea7ad51c8a74f92
th-tr-app <t.hrs001@gmail.com>
2026-03-01 17:33:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ with st.sidebar.expander("📅 市場カレンダー設定"):
 # サイト名＆サブタイトルをサイドバーの最下部に配置
 st.sidebar.markdown(
     """
-    <div style='margin-top: 50px; text-align: lleft;'>
+    <div style='margin-top: 50px; text-align: left;'>
         <h6 style='color: #ffffff; margin-bottom: 0;'>FORE CASTER</h6>
         <p style='font-size: 0.8rem; color: #ffffff;'>All-in-one Day trade manager / ver 4.98</p>
     </div>
