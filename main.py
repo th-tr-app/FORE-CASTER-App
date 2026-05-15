7b1936577cf4a9070a12f0c0b5403cff2c9dbbdc
th-tr-app <t.hrs001@gmail.com>
2026-05-15 12:20:04 +0900

    Update version number in sidebar markdown

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ st.sidebar.markdown(
     """
     <div style='margin-top: 50px; text-align: left;'>
         <h6 style='color: #ffffff; margin-bottom: 0;'>FORE CASTER</h6>
-        <p style='font-size: 0.8rem; color: #ffffff;'>All-in-one Day trade manager / ver 5.25</p>
+        <p style='font-size: 0.8rem; color: #ffffff;'>All-in-one Day trade manager / ver 5.27</p>
     </div>
     """, 
     unsafe_allow_html=True
